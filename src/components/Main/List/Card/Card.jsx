83a594a300f31718bb89a9186451cb4f6b9b1706
CARLOS <carl.gomezro@gmail.com>
2025-11-25 16:45:52 +0100

    fixedCard

--- src/components/Main/List/Card/Card.jsx
+++ src/components/Main/List/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Card = ({ todo, onDelete, onToggleDone, onEdit }) => {
+  if (!todo) return null
   return (
     <div className={`todo-item ${todo.isDone ? "done" : ""}`}>
       <div className="todo-main">
